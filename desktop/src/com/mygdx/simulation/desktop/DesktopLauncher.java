d7de2166094c02b066f9b484e8626401e5df6531
Gary Black <gar2000b@yahoo.com>
2016-11-19 12:18:49 -0500

    init commit

---
+++ desktop/src/com/mygdx/simulation/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.simulation.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.simulation.TransportSimulation;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TransportSimulation(), config);
+	}
+}
